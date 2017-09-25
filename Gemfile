f41add3f0c0782609435469d1cbf1e08869d1871
github username <github email address>
2017-09-25 11:25:04 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.2.5'
+gem 'byebug'
+gem 'rails'
 gem 'sqlite3'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
@@ -17,4 +18,3 @@ end
 group :development do
   gem 'web-console', '~> 2.0'
 end
-
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_click_bait
+
+
+  def is_click_bait
+
+    !self.title && return
+
+    click_array = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+    click_array.each do |phrase|
+      self.title.include?(phrase) && return
+
+    end
+    self.errors[:title] << "Title is not clicbaitey enough you noob"
+
+  end
+
+
+
 end
+
+# self.errors[:title] << "Title is not clicbaitey enough you noob"
